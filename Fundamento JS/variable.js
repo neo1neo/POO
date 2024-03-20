d1217be014afa4eb501e48e0995efb7000a3dc61
neo1neo <diegoechenagucia@liceovvh.cl>
2024-03-20 10:28:02 -0300

    ok

--- Fundamento JS/variable.js
+++ Fundamento JS/variable.js
@@ -1,16 +1,16 @@
 //Ejercicios Creacion de variables
-
+console.log("Bien enlazado");
 //Una variable global de tipo var
 var global = "Global";
 //Variable Local
 let local = "Local";
 //Constante
-const constante = "Constante"
+const constante = "Constante";
 let int = 4;
 //Variable NumeroDecimal
 let float = 33.3;
 //Variable CadenaTexto
-let string = "CadenaTexto"
+let string = "CadenaTexto";
 //Variable Booleana
 let bool = true;
 //Variable Arreglo
@@ -27,5 +27,5 @@ function sumar(num1, num2){
 };
 
 
-console.log("Variables: Global " + global + " Local" + local + " - Constante: " + constante + " NumeroEntero " + int + " NumeroDecimal " + float + " CadenaTexto " + string + " Booleana "
-+ bool + " Arreglo " + array + " Objeto " + Objeto + " Undefinida" + undefinida + " Nula " + null + " Operación simple" + sumar(10,7));
+console.log("Variables: Global " + global + "\n Local " + local + "\n - Constante: " + constante + "\n NumeroEntero " + int + "\n NumeroDecimal " + float + "\n CadenaTexto " + string + "\n Booleana "
++ bool + "\n Arreglo " + array + "\n Objeto " + JSON.stringify(Objeto) + "\n Undefinida " + undefinida + "\n Nula " + null + "\n Operación simple " + sumar(10,7));
